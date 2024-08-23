79dd1041bf75fdc91e9613a4c49ad5f42efef48d
Jignesh Desai <150763219+jigneshdesai9094@users.noreply.github.com>
2024-08-23 15:01:21 +0530

    Add files via upload

---
+++ P1.java
@@ -0,0 +1,32 @@
+class P1
+{
+  public static void main(String args[])
+  {
+    int arr[]=new int[args.length];
+	int last=arr.length-1;
+	System.out.println("Before Sorting ::");
+    for(int i=0;i<args.length;i++)
+	{
+	  arr[i]=Integer.parseInt(args[i]);
+	  System.out.print(arr[i]+" ");
+	}
+	for(int i=0;i<arr.length-1;i++)
+	{
+		for(int j=0;j<last;j++)
+		{
+			if(arr[j]>arr[j+1])
+			{
+				arr[j]=arr[j]+arr[j+1];
+				arr[j+1]=arr[j]-arr[j+1];
+				arr[j]=arr[j]-arr[j+1];
+			}
+		}
+		last--;
+	}
+	System.out.println("\nAfter sorting ::");
+    for(int i=0;i<arr.length;i++)
+         System.out.print(arr[i]+" ");
+    System.out.println("\nMinimum Value : "+arr[0]+" Maximum Value : "+arr[arr.length-1]);	 
+  }
+ 
+}
