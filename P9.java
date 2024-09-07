1f9a9f222546d3f364a6145a4d157226d73469a1
Jignesh Desai <150763219+jigneshdesai9094@users.noreply.github.com>
2024-09-07 12:41:29 +0530

    Update P9.java

--- P9.java
+++ P9.java
@@ -1,6 +1,8 @@
 abstract class shape
 {
-  private double v1,v2;
+  int v1,v2;
+  shape()
+  {}
   shape(int v1,int v2)
   {
       this.v1=v1;
@@ -27,21 +29,59 @@ class Rectangle extends shape
     }
     double area()
     {
-        
+        return v1*v2;
+    }
+    double perimeter()
+    {
+        return 2*(v1+v2);
     }
 }
 class Triangle extends shape
 {
+    int v3;
     Triangle()
     {
-        
+       v1=10;
+       v2=v1;   
+       v3=13;
     }
     Triangle(int v1,int v2)
     {
-        super(v1,v2);
+        super(v1);
+        v3=v2;
+    }
+    double area()
+    {
+        return (v1*v3)/2;
+    }
+    double perimeter()
+    {
+        return v1+v2+v3;
     }
 }
+
 class Circle extends shape
 {
+    Circle()
+    {
+        v2=5;
+    }
+    Circle(int r)
+    {
+        super(r);
+    }
+    double area()
+    {
+        return 3.14*v2*v2;
+    }
+    double perimeter()
+    {
+        return 2*3.14*v2;
+    }
+}
+public class P9
+{
+  public static void main(String[] args) {
     
+  }
 }
