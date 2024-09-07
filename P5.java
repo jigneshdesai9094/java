d8c10c683b1d48ca6a887212571f7cb2b895cf33
Jignesh Desai <150763219+jigneshdesai9094@users.noreply.github.com>
2024-09-07 10:54:27 +0530

    Update P5.java

--- P5.java
+++ P5.java
@@ -1,8 +1,30 @@
-class P5
+import java.util.*;
+public class P5
 {
+    public static void initialize_default(int arr1[][],int arr2[][],int row,int col)
+   {
+       int v=1;
+       for(int i=0;i<row;i++)
+       {
+           for(int j=0;j<col;j++)
+           {
+               arr1[i][j]=v;
+               v++;
+               arr2[i][j]=v;
+               v++;
+           }
+       }
+   }
 	public static void main(String args[])
 	{
-		int arr1[][]={{5,6},{7,8}},arr2[][]={{1,2},{3,4}};
+	    Scanner s1=new Scanner(System.in);
+	    int row,col;
+	    System.out.println("\nEnter Matrix row and column  : ");
+	    row=s1.nextInt();
+	    col=s1.nextInt();
+	    
+		int arr1[][]=new int[row][col],arr2[][]=new int[row][col];
+		initialize_default(arr1,arr2,row,col);
 		System.out.println(" Two Matrix Follows");
 		System.out.println("Matrix - 1");
 		for(int i=0;i<arr1.length;i++)
@@ -22,7 +44,24 @@ class P5
 			}
 			System.out.println();
 		}
-		
+		while(true)
+		{
+		    System.out.println("\n1.Own Intialize Matrix");
+		    System.out.println("2.Addition");
+		    System.out.println("3.Substration");
+		    System.out.println("4.Multiplication");
+		    System.out.println("5.Exit");
+		    System.out.println("\nEnter your choice : ");
+		    int choice=s1.nextInt();
+		    switch(choice)
+		    {
+		        case 1:System.out.println("\n1.Intialize Matrix 1");
+		               System.out.println("2.Intialize Matrix 2");
+		               System.out.println("\nEnter your choice");
+		               
+		    }
+		}
+		/*
 		System.out.println("\nAddition Of Above Two Matrix");
 			for(int i=0;i<arr1.length;i++)
 		{
@@ -55,5 +94,6 @@ class P5
 			}
 			System.out.println();
 		}
+		*/
 	}
 }
