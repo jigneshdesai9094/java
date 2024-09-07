2c8c9a1cf0c8b3285fc502ac1e97d0b2dadde6d2
Jignesh Desai <150763219+jigneshdesai9094@users.noreply.github.com>
2024-09-07 13:28:46 +0530

    Update P9.java

--- P9.java
+++ P9.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 abstract class shape
 {
   int v1,v2;
@@ -81,7 +83,106 @@ class Circle extends shape
 }
 public class P9
 {
+static Scanner sc=new Scanner(System.in);
+  public static void operation(shape s)
+  {
+    while (true)
+    {
+        System.out.println("1.Area");
+        System.out.println("2.Perimeter");
+        System.out.println("3.exit");
+        System.out.println("\nEnter operation no : ");
+        int c=sc.nextInt();
+        switch(c)
+        {
+            case 1:System.out.println("Area : "+s.area());
+                   break;
+            case 2:System.out.println("Perimeter : "+s.perimeter());
+                    break;
+            case 3:return;
+            default:System.out.println("please,enter valid choice");
+        }
+    }
+  }
   public static void main(String[] args) {
-    
+  
+    System.out.println("\nVarious shape to perform operation : ");
+    System.out.println("1.Rectangle");
+    System.out.println("2.Triangle");
+    System.out.println("3.Circle");
+    System.out.println("4.exit");
+    System.out.println("Enter Shape no to perform operation  : ");
+    int choice=sc.nextInt();
+    if(choice==1)
+    {
+       System.out.println("1.User Default size Rectangle");
+       System.out.println("2.Use Specific size Rectangle");
+       System.out.println("Enter No Specific Rectange Use : ");
+       int c=sc.nextInt();
+       if(c==1)
+       {
+         Rectangle  r = new Rectangle();
+         operation(r);
+       }
+       else if(c==2)
+       {
+         int l,b;
+         System.out.println("\nEnter l and b : ");
+         l=sc.nextInt();
+         b=sc.nextInt();
+         Rectangle r=new Rectangle(l,b);
+         operation(r);
+       }
+       else
+         System.out.println("please,enter valid choice");
+    }
+    else if(choice==2)
+    {
+        System.out.println("1.User Default size Triangle");
+       System.out.println("2.Use Specific size Triangle");
+       System.out.println("Enter No Specific Triangle Use : ");
+       int c=sc.nextInt();
+       if(c==1)
+       {
+         Triangle  r = new Triangle();
+         operation(r);
+       }
+       else if(c==2)
+       {
+         int l,b;
+         System.out.println("\nEnter height and base : ");
+         l=sc.nextInt();
+         b=sc.nextInt();
+         Triangle r=new Triangle(l,b);
+         operation(r);
+       }
+       else
+         System.out.println("please,enter valid choice");
+    }
+    else if(choice==3)
+    {
+        System.out.println("1.User Default size Circle");
+        System.out.println("2.Use Specific size Circle");
+        System.out.println("Enter No Specific Circle Use : ");
+        int c=sc.nextInt();
+        if(c==1)
+        {
+          Circle  r = new Circle();
+          operation(r);
+        }
+        else if(c==2)
+        {
+          int l,b;
+          System.out.println("\nEnter Circle Radius ");
+          l=sc.nextInt();
+        
+          Circle r=new Circle(l);
+          operation(r);
+        }
+        else
+          System.out.println("please,enter valid choice");
+    }
+    else
+      System.out.println("\nPlease,entr valid choice");
   }
 }
