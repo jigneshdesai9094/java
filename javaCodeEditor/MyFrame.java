f93e421e7772d97b0ad3e833e8a70a261fa1694b
Jignesh Desai <150763219+jigneshdesai9094@users.noreply.github.com>
2024-09-29 11:57:42 +0530

    Add files via upload

--- javaCodeEditor/MyFrame.java
+++ javaCodeEditor/MyFrame.java
@@ -1,45 +1,91 @@
-import java.awt.BorderLayout;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import javax.swing.text.*;
 
-
-public class MyFrame extends JFrame implements KeyListener {
-     JMenuBar jMenuBar;
-     JMenu jMenu,jMenu2;
-     JTextArea jTextArea;
-     MyFrame()
-     {
-        // Menu Bar
-        
-        jMenuBar=new JMenuBar();
-        jMenu=new JMenu("File");
-        jMenu2=new JMenu("Edit");
-
-        jMenuBar.add(jMenu);
-        jMenuBar.add(jMenu2);
-        this.setJMenuBar(jMenuBar);
-         
-       // Textarea
-        jTextArea=new JTextArea();
-        this.add(jTextArea,BorderLayout.CENTER);
-        jTextArea.addKeyListener(this);
-     }   
-     @Override
+class MyFrame1 extends JFrame implements KeyListener
+{ 
+    JTextPane jt=new JTextPane();
+    StyledDocument doc ;
+    SimpleAttributeSet style;
+ 
+    MyFrame1()
+    {
+        setTitle("Java Code Editor");
+        setSize(600, 400);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(jt,BorderLayout.CENTER);
+        jt.addKeyListener(this);
+        doc = jt.getStyledDocument();
+        style = new SimpleAttributeSet();
+        StyleConstants.setForeground(style, Color.BLUE);
+    }
+    @Override
      public void keyPressed(KeyEvent e) {
-         // TODO Auto-generated method stub
+        // try
+        // {
+           
+        //    String arr[]={"public"};
+        //    int pos = jt.getCaretPosition();
+        //    System.out.println("Pos ="+pos);
+        //    for(String a:arr)
+        //    {
+        //     int beg=pos-(a.length()-1);
+        //     if(beg>=0)
+        //      System.out.println(jt.getText(0,4));
+        //      //if(a.length()-1>=0)
+        //        //System.out.println(jt.getText(a.length()-1,a.length()));
+        //    }
+        // }catch(Exception err)
+        // {
+        //     err.printStackTrace();
+        // }
          
      }
+     
      @Override
      public void keyReleased(KeyEvent e) {
+        try
+        {
+           
+           String arr[]={"public","static","void","if","for","switch"};
+           int pos = jt.getCaretPosition();
+           System.out.println("Pos ="+pos);
+           for(String a:arr)
+           {
+            int beg=pos-(a.length());
+            
+            if(beg>=0)
+            {
+             System.out.println(jt.getText(beg,a.length()));
+             String s=jt.getText(beg,a.length());
+             if((s.trim()).equals(a))
+              doc.setCharacterAttributes(beg, a.length(), style, false);
+            }
+             //if(a.length()-1>=0)
+               //System.out.println(jt.getText(a.length()-1,a.length()));
+           }
+        }catch(Exception err)
+        {
+            err.printStackTrace();
+        }
+         
+     }
+
+     @Override
+     public void keyTyped(KeyEvent e) {
          // TODO Auto-generated method stub
          
      }
-    @Override
-    public void keyTyped(KeyEvent e) {
-        // TODO Auto-generated method stub
-        
-    }
+}
+public class MyFrame{
+
+     public static void main(String[] args) {
+        MyFrame1 f=new MyFrame1();
+     
+     }
+     
+    
+      
 }
